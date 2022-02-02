1d31e02216646dae38d6ce80b5fc9d1c8ee35844
Jerome Dalbert <jerome.dalbert@gmail.com>
2022-02-01 17:57:36 -0800

    Allow dots in route IDs

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ RailsAdmin::Engine.routes.draw do
       RailsAdmin::Config::Actions.all(:collection).each { |action| match "/#{action.route_fragment}", action: action.action_name, as: action.action_name, via: action.http_methods }
       post '/bulk_action', action: :bulk_action, as: 'bulk_action'
       scope ':id' do
-        RailsAdmin::Config::Actions.all(:member).each { |action| match "/#{action.route_fragment}", action: action.action_name, as: action.action_name, via: action.http_methods }
+        RailsAdmin::Config::Actions.all(:member).each { |action| match "/#{action.route_fragment}", action: action.action_name, as: action.action_name, via: action.http_methods, constraints: { id: /[^\/]+/ } }
       end
     end
   end
